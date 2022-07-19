4b0c689a0af473ca352f56e7e54d106f018555a7
Eshy10 <adewalebose15@gmail.com>
2022-07-19 02:21:28 +0100

    populate the home page with videos from post

---
+++ components/VideoCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Video} from '../types'
+
+type VideoCardProps = {
+    video: Video,
+}
+
+const VideoCard = ({video}: VideoCardProps) => {
+  return (
+    <div>VideoCard</div>
+  )
+}
+
+export default VideoCard
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,28 @@
-import type { NextPage } from 'next'
-import axios from 'axios'
+import type { NextPage } from "next";
+import axios from "axios";
+import { Video } from "../types";
+import VideoCard from "../components/VideoCard";
 
-const Home: NextPage = () => {
-  return (
-    <div>
-    Tiktik
-    </div>
-  )
+interface IProps {
+  videos: Video[];
 }
 
-export default Home
+const Home = ({ videos }: IProps) => {
+  console.log(videos);
+  return (
+    <div className="flex flex-col gap-10 videos h-full">
+      {videos.length ? (
+        videos.map((video) => <VideoCard key={video._id} video={video} />)
+      ) : (
+        <p>No videos found</p>
+      )}
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const { data } = await axios.get("http://localhost:3000/api/post");
+  return { props: { videos: data } };
+};
+
+export default Home;
