df2160463fbabfa376305e6e04c867301fbc2fae
Eshy10 <adewalebose15@gmail.com>
2022-08-08 16:04:15 +0100

    chore: refactor show accounts

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import { GoVerified } from 'react-icons/go';
-import Link from 'next/link';
-import axios from 'axios';
+import React, { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { GoVerified } from "react-icons/go";
+import Link from "next/link";
+import axios from "axios";
 
-import NoResults from '../../components/NoResult';
-import VideoCard from '../../components/VideoCard';
-import useAuthStore from '../../store/authStore';
-import { BASE_URL } from '../../utils';
-import { IUser, Video } from '../../types';
+import NoResults from "../../components/NoResult";
+import VideoCard from "../../components/VideoCard";
+import useAuthStore from "../../store/authStore";
+import { BASE_URL } from "../../utils";
+import { IUser, Video } from "../../types";
 
 const Search = ({ videos }: { videos: Video[] }) => {
   const [isAccounts, setIsAccounts] = useState(false);
@@ -18,9 +18,11 @@ const Search = ({ videos }: { videos: Video[] }) => {
   const router = useRouter();
   const { searchTerm }: any = router.query;
 
-  const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
-  const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
-  const searchedAccounts = allUsers?.filter((user: IUser) => user.userName.toLowerCase().includes(searchTerm));
+  const accounts = isAccounts ? "border-b-2 border-black" : "text-gray-400";
+  const isVideos = !isAccounts ? "border-b-2 border-black" : "text-gray-400";
+  const searchedAccounts = allUsers?.filter((user: IUser) =>
+    user.userName.toLowerCase().includes(searchTerm)
+  );
 
   const showVideos = () => {
     if (videos.length > 0) {
@@ -30,47 +32,61 @@ const Search = ({ videos }: { videos: Video[] }) => {
     } else {
       return <NoResults text={`No Video Results for ${searchTerm}`} />;
     }
-  }
-  
+  };
+
+  const showAccounts = () => {
+    if (searchedAccounts?.length > 0) {
+      return searchedAccounts.map((user: IUser, idx: number) => (
+        <Link key={idx} href={`/profile/${user._id}`}>
+          <div className=" flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200">
+            <div>
+              <Image
+                width={50}
+                height={50}
+                className="rounded-full"
+                alt="user-profile"
+                src={user.image}
+              />
+            </div>
+            <div>
+              <div>
+                <p className="flex gap-1 items-center text-lg font-bold text-primary">
+                  {user.userName} <GoVerified className="text-blue-400" />
+                </p>
+                <p className="capitalize text-gray-400 text-sm">
+                  {user.userName}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      ));
+    } else {
+      return <NoResults text={`No Account Results for ${searchTerm}`} />;
+    }
+  };
+
   return (
-    <div className='w-full  '>
-      <div className='flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full'>
-        <p onClick={() => setIsAccounts(true)} className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}>
+    <div className="w-full  ">
+      <div className="flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full">
+        <p
+          onClick={() => setIsAccounts(true)}
+          className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}
+        >
           Accounts
         </p>
-        <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
+        <p
+          className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`}
+          onClick={() => setIsAccounts(false)}
+        >
           Videos
         </p>
       </div>
       {isAccounts ? (
-        <div className='md:mt-16'>
-          {searchedAccounts.length > 0 ? (
-            searchedAccounts.map((user: IUser, idx: number) => (
-              <Link key={idx} href={`/profile/${user._id}`}>
-                <div className=' flex gap-3 p-2 cursor-pointer font-semibold rounded border-b-2 border-gray-200'>
-                  <div>
-                    <Image width={50} height={50} className='rounded-full' alt='user-profile' src={user.image}/>
-                  </div>
-                  <div>
-                    <div>
-                      <p className='flex gap-1 items-center text-lg font-bold text-primary'>
-                        {user.userName} <GoVerified className='text-blue-400' />
-                      </p>
-                      <p className='capitalize text-gray-400 text-sm'>
-                        {user.userName}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </Link>
-            ))
-          ) : (
-            <NoResults text={`No Account Results for ${searchTerm}`} />
-          )}
-        </div>
+        <div className="md:mt-16">{showAccounts()}</div>
       ) : (
-        <div className='md:mt-16 flex flex-wrap gap-6 md:justify-start '>
-            {showVideos()}
+        <div className="md:mt-16 flex flex-wrap gap-6 md:justify-start ">
+          {showVideos()}
         </div>
       )}
     </div>
